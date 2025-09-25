8b329e444d14b7783eb436e89278ebb055b9923c
JJPontes <16lj27@hotmail.com>
2025-09-24 23:36:57 -0300

    refactor: Unifying the cards into one, but keeping a parameter to switch between the Grid version and the List version

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,7 @@
-import CardGrid from '../presenters/components/cardsGrid';
-
 function App() {
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center p-8">
-      <CardGrid
-        title={
-          'em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos '
-        }
-        description={
-          'em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-        }
-        author={'julio'}
-        createDate={'18/09/2025'}
-      />
+      <h1>Blog Challenge - SPA</h1>
     </div>
   );
 }
---
+++ src/presenters/components/postCard/index.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useRef } from 'react';
+import Hero from '../../assets/Hero.png';
+import { DotsThreeVertical } from 'phosphor-react';
+import { FormatUpperAndCharacterLimiter } from '../utils/format';
+import { useClickOutside } from '../../../hooks/click/clickAway';
+
+interface CardProps {
+  title: string;
+  description: string;
+  author: string;
+  createDate?: string;
+  isLandscape?: boolean;
+}
+
+const PostCard: React.FC<CardProps> = ({
+  title,
+  description,
+  author,
+  createDate,
+  isLandscape,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dialogRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleDialogOpen = () => setIsOpen(prev => !prev);
+
+  useClickOutside([dialogRef, buttonRef], () => setIsOpen(false));
+
+  return (
+    <div
+      className={`flex ${
+        isLandscape
+          ? 'flex-row-reverse min-w-sm max-w-5xl w-3x1'
+          : 'flex-col-reverse max-w-sm w-full'
+      }`}
+    >
+      {/* Imagem */}
+      <div
+        className={`h-48 ${
+          isLandscape ? 'h-auto w-1/3 rounded-r-lg' : 'rounded-b-lg'
+        } flex-none bg-cover bg-center text-center overflow-hidden`}
+        style={{ backgroundImage: `url(${Hero})` }}
+        title={title}
+      ></div>
+
+      {/* Conteúdo */}
+      <div
+        className={`border border-[#DFDFDF] bg-white p-4 flex flex-col justify-between leading-normal
+        ${isLandscape ? 'rounded-l-lg' : 'rounded-t-lg'}`}
+      >
+        <div className="mb-8">
+          {isOpen && (
+            <div
+              ref={dialogRef}
+              className={`absolute ${isLandscape ? 'mt-8 ml-140' : 'mt-8 ml-45'} bg-white rounded-[5px] border border-[#DFDFDF] shadow-md`}
+            >
+              <div className="min-w-[80px]">
+                <ul className="divide-y divide-[#DFDFDF] text-center">
+                  <li className="cursor-pointer py-1 text-black hover:bg-gray-300 rounded-t-[5px]">
+                    Editar
+                  </li>
+                  <li className="cursor-pointer py-1 text-red-500 hover:bg-gray-300 rounded-b-[5px]">
+                    Deletar
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+
+          <div className="flex items-center">
+            <span className="text-gray-900 font-bold text-xl mb-2 mr-3 line-clamp-2">
+              {FormatUpperAndCharacterLimiter(title)}
+            </span>
+            <button
+              ref={buttonRef}
+              onClick={handleDialogOpen}
+              className="text-black font-bold "
+            >
+              {' '}
+              <DotsThreeVertical className="w-10 h-10 -mt-10 -mr-3 cursor-pointer " />
+            </button>
+          </div>
+
+          <p className="text-gray-700 text-base  line-clamp-5 mt-3">
+            {FormatUpperAndCharacterLimiter(description)}
+          </p>
+        </div>
+
+        <div className="flex justify-between">
+          <data className="text-left italic text-gray-400">{createDate}</data>
+          <p className="text-right italic">
+            Criado por: {FormatUpperAndCharacterLimiter(author, 20)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
