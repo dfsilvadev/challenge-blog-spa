e6d331a63cbc1cdf517966003bbfffe01934411a
JJPontes <16lj27@hotmail.com>
2025-09-30 20:16:24 -0300

    refactor: Removing postcards from the home page

--- src/presenters/pages/posts.tsx
+++ src/presenters/pages/posts.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import PostCard from '../components/postCard';
 import { Funnel, SquaresFour, ListBullets } from 'phosphor-react';
 
 const Posts = () => {
@@ -60,116 +59,7 @@ const Posts = () => {
               ? 'gap-5 justify-items-centergrid-cols-1'
               : 'gap-10 grid-cols-2 md:grid-cols-3 lg:grid-cols-7 2xl:grid-cols-[repeat(auto-fit,minmax(250px,1fr))]'
           }`}
-        >
-          <PostCard
-            title={'Testandogjbjbjyyytnon bposnfomboimsgoigmdpo'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />{' '}
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />{' '}
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />{' '}
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />{' '}
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-          <PostCard
-            title={'Testando'}
-            description={
-              'Em função das demandas emergentes, o desenvolvimento contínuo de distintas formas de atuação ancora-se em pressupostos teóricos consistentes das condições financeiras e administrativas exigidas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a adoção de políticas descentralizadoras reforça a necessidade de ajustes dinâmicos dos modos de operação convencionais. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a complexidade dos estudos efetuados traz consigo um leque de implicações práticas do sistema de participação geral. Considerando as lições aprendidas, a análise aprofundada dos indicadores-chave auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. É claro que a revisão das métricas de desempenho requer um olhar atento sobre os desdobramentos das posturas dos órgãos dirigentes com relação às suas atribuições. Sob essa perspectiva, o fenômeno da Internet facilita a criação dos índices pretendidos. No escopo da atual conjuntura, o comprometimento entre as equipes estimula a padronização da fluidez dos cenários contemporâneos. Por conseguinte, a percepção das dificuldades modifica os parâmetros tradicionais de análise de alternativas às soluções ortodoxas. É importante questionar o quanto a '
-            }
-            author={'julio'}
-            createDate={'29/09/2025'}
-            isLandscape={isLandscape}
-          />
-        </div>
+        ></div>
       </div>
     </div>
   );
