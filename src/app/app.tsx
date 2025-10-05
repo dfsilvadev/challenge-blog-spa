d992fde8062ffd08f5190507b789aae165ada743
Clara Pontes <mclarapss@gmail.com>
2025-10-05 16:21:00 -0300

    Limpando app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,5 @@
-import CommentCardGrid from '../presenters/components/commentCardGrid';
-
 function App() {
-  return (
-    <div className="min-h-screen bg-background flex flex-col items-center justify-center p-8">
-      <CommentCardGrid></CommentCardGrid>
-    </div>
-  );
+  return <h1>App</h1>;
 }
 
 export default App;
